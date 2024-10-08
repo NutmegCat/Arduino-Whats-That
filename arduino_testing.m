c37f46078b624080fee7e7e20f423a5f1fcfcf88
NutmegCat <deltaechogames@gmail.com>
2024-10-08 15:41:24 -0400

    Part 2

--- arduino_testing.m
+++ arduino_testing.m
@@ -1,11 +1,3 @@
 clear
 
-a=arduino('COM5','Nano3');
-
-writeDigitalPin(a,'D13',1);
-pause(0.5);
-writeDigitalPin(a,'D13',0);
-pause(0.5);
-writeDigitalPin(a,'D13',1);
-pause(1.0);
-writeDigitalPin(a,'D13',0);
+a = arduino('COM5', 'Nano3');
