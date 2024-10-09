a28a2339755ac4f955255e130274f5869647d624
NutmegCat <deltaechogames@gmail.com>
2024-10-09 14:35:41 -0400

    format

--- arduino_testing.m
+++ arduino_testing.m
@@ -7,17 +7,13 @@ y = 0;
 frequency = 0;
 
 while (x == 4000)
-    while  (frequency < x)
-      playTone(a,'D5',frequency,1)
-      frequency = frequency + 100;
-    end
+  while  (frequency < x)
+    playTone(a,'D5',frequency,1)
+    frequency = frequency + 100;
+  end
 
-    while (frequency > y)
+  while (frequency > y)
     playTone(a,'D5',frequency,1)
     frequency = frequency - 100;
-    end
+  end
 end
-
-
-
-
