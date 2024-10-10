a3a0caa66c1f78cbba06aacbf8d7d63b06a72555
NutmegCat <deltaechogames@gmail.com>
2024-10-10 17:05:43 -0400

    changed LEDs

--- arduino_testing.m
+++ arduino_testing.m
@@ -2,10 +2,10 @@ clear
 
 a=arduino('COM5','Nano3');
 
-writeDigitalPin(a,'D13',1);
+writeDigitalPin(a,'D4',1);
 pause(0.5);
-writeDigitalPin(a,'D13',0);
+writeDigitalPin(a,'D4',0);
 pause(0.5);
-writeDigitalPin(a,'D13',1);
+writeDigitalPin(a,'D4',1);
 pause(1.0);
-writeDigitalPin(a,'D13',0);
+writeDigitalPin(a,'D4',0);
